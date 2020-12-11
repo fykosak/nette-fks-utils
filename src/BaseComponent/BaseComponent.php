1a9a4b849004e0e12f5a2fa4674bd27096b24083
Michal Mišo Červeňák <miso@fykos.cz>
2020-12-11 23:10:52 +0100

    remove startup and render method

--- src/BaseComponent/BaseComponent.php
+++ src/BaseComponent/BaseComponent.php
@@ -9,14 +9,12 @@ use Nette\Bridges\ApplicationLatte\Template;
 use Nette\DI\Container;
 
 abstract class BaseComponent extends Control {
-
     protected Container $container;
     protected GettextTranslator $translator;
 
     public function __construct(Container $container) {
         $this->container = $container;
         $container->callInjects($this);
-        $this->startUp();
     }
 
     protected function getContext(): Container {
@@ -27,21 +25,10 @@ abstract class BaseComponent extends Control {
         $this->translator = $translator;
     }
 
-    public function render(): void {
-        $this->beforeRender();
-        $this->getTemplate()->render();
-    }
-
     protected function createTemplate(): ITemplate {
         /** @var Template $template */
         $template = parent::createTemplate();
         $template->setTranslator($this->translator);
         return $template;
-    }
-
-    protected function beforeRender(): void {
-    }
-
-    protected function startUp(): void {
     }
 }
