4043d85a5dfe399e092e5417a2c6439542e03d67
Michal Mišo Červeňák <miso@fykos.cz>
2021-10-11 20:09:12 +0200

    nav component as DI dependent component

--- src/UI/Navigation/NavigationItemComponent.php
+++ src/UI/Navigation/NavigationItemComponent.php
@@ -2,13 +2,13 @@
 
 namespace Fykosak\Utils\UI\Navigation;
 
-use Nette\Application\UI\Control;
+use Fykosak\Utils\BaseComponent\BaseComponent;
 
 /**
  * Compatible with bootstrap >=5.0
  * tested version (5.0,5.1)
  */
-class NavigationItemComponent extends Control
+class NavigationItemComponent extends BaseComponent
 {
     public function render(NavItem $item): void
     {
