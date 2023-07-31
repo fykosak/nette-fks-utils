4fcf013f3c4c90656a09894feb26271b8c88714b
Michal Mišo Červeňák <miso@fykos.cz>
2023-07-31 20:26:05 +0200

    LocalizedString

--- src/Localization/LocalizedString.php
+++ src/Localization/LocalizedString.php
@@ -4,31 +4,43 @@ declare(strict_types=1);
 
 namespace Fykosak\Utils\Localization;
 
+/**
+ * @template L of string
+ */
 class LocalizedString
 {
     /**
-     * @phpstan-var array<string,string>
+     * @phpstan-var array<L,string>
      */
     private array $texts;
 
     /**
-     * @phpstan-param array<string,string> $texts
+     * @phpstan-param array<L,string> $texts
      */
     public function __construct(array $texts)
     {
         $this->texts = $texts;
     }
 
+    /**
+     * @phpstan-param L $lang
+     */
     public function __get(string $lang): ?string
     {
         return $this->getText($lang);
     }
 
+    /**
+     * @phpstan-param L $lang
+     */
     public function getText(string $lang): ?string
     {
         return $this->texts[$lang] ?? null;
     }
 
+    /**
+     * @phpstan-return array<L,string>
+     */
     public function __serialize(): array
     {
         return $this->texts;
