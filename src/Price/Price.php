841652d815093dedadd0a57652fe9eef7e735428
Michal Mišo Červeňák <miso@fykos.cz>
2024-10-27 20:26:39 +0100

    fix ===

--- src/Price/Price.php
+++ src/Price/Price.php
@@ -20,7 +20,7 @@ final class Price
      */
     public function add(Price $price): void
     {
-        if ($this->currency->value !== $price->getCurrency()->value) {
+        if ($this->currency !== $price->getCurrency()) {
             throw new \LogicException('Currencies are not a same');
         }
         $this->amount += $price->getAmount();
