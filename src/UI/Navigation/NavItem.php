c176e824353f0cf484b4e5b9270a929c0d6f088a
Michal Mišo Červeňák <miso@fykos.cz>
2025-09-10 22:42:53 +0200

    localized Nav

--- src/UI/Navigation/NavItem.php
+++ src/UI/Navigation/NavItem.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace Fykosak\Utils\UI\Navigation;
 
+use Fykosak\Utils\UI\LocalizedTitle;
 use Fykosak\Utils\UI\Title;
 
+/**
+ * @phpstan-template TLang of string
+ */
 class NavItem
 {
     /**
      * @phpstan-param array<string,scalar> $linkParams
+     * @phpstan-param Title|LocalizedTitle<TLang> $title
      * @param NavItem[] $children
      */
     public function __construct(
-        public readonly Title $title,
+        public readonly Title|LocalizedTitle $title,
         public readonly string $destination = '#',
         public readonly array $linkParams = [],
         public readonly array $children = [],
